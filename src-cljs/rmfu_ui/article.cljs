5ace7bb8852ede0d14ed4c8794de9a1126564c28
Zachary Rose <zachary.f.rose@gmail.com>
2015-11-08 13:51:17 -0700

    Addition of content to article.cljs

--- src-cljs/rmfu_ui/article.cljs
+++ src-cljs/rmfu_ui/article.cljs
@@ -1,4 +1,106 @@
 (ns rmfu-ui.article)
 
 (defn article []
-  [:h1 "Article"])
+[:div.container.jumbotron.largemain
+ [:div.row
+  [:div.col-lg-12
+   [:h1
+    "Article"]
+   [:div.pre-scrollable
+    [:h4.col-lg-9.articleheading
+     "New Legislation that will impact you!"]
+    [:p.col-lg-3.date
+     "October 1, 2015"]
+    [:p
+     "    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute\n              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n              "
+     [:br]
+     [:br]
+     "    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute\n              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n              "
+     [:br]
+     [:br]
+     "    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute\n              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n              "
+     [:br]
+     [:br]
+     "    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute\n              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n              "
+     [:br]
+     [:br]
+     "    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute\n              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n              "
+     [:br]
+     [:br]
+     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure\n              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n              "
+     [:br]
+     [:br]
+     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure\n              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n              "
+     [:br]
+     [:br]
+     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure\n              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n              "
+     [:br]
+     [:br]
+     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure\n              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n            "]]
+   [:br]
+   [:div.greybox
+    [:ul.list-inline
+     [:li
+      [:button.btn.btn-default
+       {:type "button", :value "add"}
+       "Collapse"]]
+     [:li
+      [:button.btn.btn-default
+       {:type "button", :value "add"}
+       "Topics"]]
+     [:li
+      [:button.btn.btn-default
+       {:type "button", :value "add"}
+       "Comments"]]
+     [:li
+      [:button.btn.btn-default
+       {:type "button", :value "add"}
+       "Share"]]
+     [:li
+      [:button.btn.btn-default
+       {:type "button", :value "add"}
+       "Archive"]]]
+    [:ul.list-inline
+     [:li
+      [:a
+       {:href "#"}
+       "Fertilizer"]]
+     [:li
+      [:a
+       {:href "#"}
+       "Legislation"]]
+     [:li
+      [:a
+       {:href "#"}
+       "Vegetables"]]
+     [:li
+      [:a
+       {:href "#"}
+       "Grain"]]
+     [:li
+      [:a
+       {:href "#"}
+       "Fruit"]]
+     [:li
+      "80246 +5 Miles"]]
+    [:div.pre-scrollable.height150
+     [:div.col-lg-3.date
+      "Oct 1, 2015"]
+     [:div.col-lg-9.comment
+      "Joe - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididul Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
+     [:div.col-lg-3.date
+      "Oct 1, 2015"]
+     [:div.col-lg-9.comment
+      "Bob - Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et!"]
+     [:div.col-lg-3.date
+      "Oct 1, 2015"]
+     [:div.col-lg-9.comment
+      "Bill - Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
+     [:div.col-lg-3.date
+      "Oct 1, 2015"]
+     [:div.col-lg-9.comment
+      "Roscoe - Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+     [:div.col-lg-3.date
+      "Oct 1, 2015"]
+     [:div.col-lg-9.comment
+      "Tiberius - Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."]]]]]])
