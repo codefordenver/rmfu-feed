b0713c87bec7f14fefbec9a985b48148c691abef
Zachary Rose <zachary.f.rose@gmail.com>
2015-11-08 21:40:43 -0700

    Added dategroup to article.cljs

--- src-cljs/rmfu_ui/article.cljs
+++ src-cljs/rmfu_ui/article.cljs
@@ -7,10 +7,11 @@
    [:h1.text-center
     "Article"]
    [:div.pre-scrollable.borderbox
-    [:h4.col-lg-9.articleheading
-     "New Legislation that will impact you!"]
-    [:p.col-lg-3.date
-     "October 1, 2015"]
+     [:div.dategroup
+      [:h4.articleheading
+       "New Legislation that will impact you!"]
+      [:p.date
+       "October 1, 2015"]]
     [:p
      "    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute\n              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n              "
      [:br]
