952d28a6cf57d1708c05c0e6761c5fffb8bf8bea
dviramontes <dviramontes@gmail.com>
2015-11-15 23:43:08 -0700

    fixes :className on feed.cljs

--- src-cljs/rmfu_ui/feed.cljs
+++ src-cljs/rmfu_ui/feed.cljs
@@ -4,116 +4,114 @@
 (defn feed []
   [:div
    [nav]
-   [:div {:class "container jumbotron largemain"}
-        [:div {:class "row"}
-         [:div {:class "col-lg-12"}
-          [:h1 {:class "displayinline"} "Feeds "] " "
-          [:h4 {:class "displayinline greytext"} " {{Inspiring tagline goes here}}"]
-          [:hr]
-          [:h4 "Articles"]
-          [:div {:class "articlelisting"}
-           [:p {:class "articlelink"}
-            [:a {:href "#"} "NFU Responds to crop insurance budget deal "
-             [:span {:class "label label-default label-pill"} "14"]]]
-           [:p {:class "user"} "Bill Stevenson "
-            [:small "November 1, 2015"] " "
-            [:a {:href "#"}
-             [:span {:class "label label-primary"} "All"]]] ]
-          [:div {:class "articlelisting"}
-           [:p {:class "articlelink"}
-            [:a {:href "#"} "Emerging leaders tour Ring-A-Ding Farm "
-             [:span {:class "label label-default label-pill"} "14"]]]
-           [:p {:class "user"} "Bill Stevenson "
-            [:small "November 1, 2015"] " "
-            [:a {:href "#"}
-             [:span {:class "label label-primary"} "Farming Technology"] " "
-             [:span {:class "label label-primary"} "Fruit/Veg"]]] ]
-          [:div {:class "articlelisting"}
-           [:p {:class "articlelink"}
-            [:a {:href "#"} "Felis euismod semper eget lacinia "
-             [:span {:class "label label-default label-pill"} "#replies"]]]
-           [:p {:class "user"} "username "
-            [:small "Month day, year"] " "
-            [:a {:href "#"}
-             [:span {:class "label label-primary"} "Label"]]] ]
-          [:div {:class "articlelisting"}
-           [:p {:class "articlelink"}
-            [:a {:href "#"} "Felis euismod semper eget lacinia "
-             [:span {:class "label label-default label-pill"} "#replies"]]]
-           [:p {:class "user"} "username "
-            [:small "Month day, year"] " "
-            [:a {:href "#"}
-             [:span {:class "label label-primary"} "Label"]]] ]
-          [:div {:class "articlelisting"}
-           [:p {:class "articlelink"}
-            [:a {:href "#"} "Felis euismod semper eget lacinia "
-             [:span {:class "label label-default label-pill"} "#replies"]]]
-           [:p {:class "user"} "username "
-            [:small "Month day, year"] " "
-            [:a {:href "#"}
-             [:span {:class "label label-primary"} "Label"]]] ]
-          [:div {:class "articlelisting"}
-           [:p {:class "articlelink"}
-            [:a {:href "#"} "Felis euismod semper eget lacinia "
-             [:span {:class "label label-default label-pill"} "#replies"]]]
-           [:p {:class "user"} "username "
-            [:small "Month day, year"] " "
-            [:a {:href "#"}
-             [:span {:class "label label-primary"} "Label"]]] ]
-          [:h5 "Latest Comments "
-           [:small
-            [:small "what your neightbors are saying"]]]
-          [:div {:class "table-responsive"}
-           [:table {:class "table table-striped table-sm articletable"}
-            [:thead
-             [:tr
-              [:th "User"]
-              [:th "Article"]
-              [:th "Label"]
-              [:th "Date\n                 "]] ]
-            [:tbody
-             [:tr
-              [:td "Jen"]
-              [:td "Farmer&#39;s Market Planning"]
-              [:td
-               [:span {:class "label label-default"} "Fruit/Veg"]]
-              [:td "November 1, 2015"] ]
-             [:tr
-              [:td "Katie"]
-              [:td "Emerging Leaders Tour Rang-A-Ding Farm"]
-              [:td
-               [:span {:class "label label-default"} "Farming Technology"] " "
-               [:span {:class "label label-default"} "Fruit/Veg"]]
-              [:td "November 1, 2015"] ]
-             [:tr
-              [:td "John"]
-              [:td "NFU Responds to Farm Bill"]
-              [:td
-               [:span {:class "label label-default"} "All"]]
-              [:td "November 1, 2015"] ]
-             [:tr {:class "table-success"}
-              [:td "Success"]
-              [:td "Article Name"]
-              [:td
-               [:span {:class "label label-default"} "Label"]]
-              [:td "Month day, year"] ]
-             [:tr {:class "table-warning"}
-              [:td "Warning"]
-              [:td "Article Name"]
-              [:td
-               [:span {:class "label label-default"} "Label"]]
-              [:td "Month day, year"] ]
-             [:tr {:class "table-danger"}
-              [:td "Danger"]
-              [:td "Article Name"]
-              [:td
-               [:span {:class "label label-default"} "Label"]]
-              [:td "Month day, year"] ]
-             [:tr {:class "table-info"}
-              [:td "Info"]
-              [:td "Article Name"]
-              [:td
-               [:span {:class "label label-default"} "Label"]]
-              [:td "Month day, year"] ] ] ] ] ] ] ]
-
- ])
+   [:div {:className "container jumbotron largemain"}
+    [:div {:className "row"}
+     [:div {:className "col-lg-12"}
+      [:h1 {:className "displayinline"} "Feeds "] " "
+      [:h4 {:className "displayinline greytext"} " {{Inspiring tagline goes here}}"]
+      [:hr]
+      [:h4 "Articles"]
+      [:div {:className "articlelisting"}
+       [:p {:className "articlelink"}
+        [:a {:href "#"} "NFU Responds to crop insurance budget deal "
+         [:span {:className "label label-default label-pill"} "14"]]]
+       [:p {:className "user"} "Bill Stevenson "
+        [:small "November 1, 2015"] " "
+        [:a {:href "#"}
+         [:span {:className "label label-primary"} "All"]]]]
+      [:div {:className "articlelisting"}
+       [:p {:className "articlelink"}
+        [:a {:href "#"} "Emerging leaders tour Ring-A-Ding Farm "
+         [:span {:className "label label-default label-pill"} "14"]]]
+       [:p {:className "user"} "Bill Stevenson "
+        [:small "November 1, 2015"] " "
+        [:a {:href "#"}
+         [:span {:className "label label-primary"} "Farming Technology"] " "
+         [:span {:className "label label-primary"} "Fruit/Veg"]]]]
+      [:div {:className "articlelisting"}
+       [:p {:className "articlelink"}
+        [:a {:href "#"} "Felis euismod semper eget lacinia "
+         [:span {:className "label label-default label-pill"} "#replies"]]]
+       [:p {:className "user"} "username "
+        [:small "Month day, year"] " "
+        [:a {:href "#"}
+         [:span {:className "label label-primary"} "Label"]]]]
+      [:div {:className "articlelisting"}
+       [:p {:className "articlelink"}
+        [:a {:href "#"} "Felis euismod semper eget lacinia "
+         [:span {:className "label label-default label-pill"} "#replies"]]]
+       [:p {:className "user"} "username "
+        [:small "Month day, year"] " "
+        [:a {:href "#"}
+         [:span {:className "label label-primary"} "Label"]]]]
+      [:div {:className "articlelisting"}
+       [:p {:className "articlelink"}
+        [:a {:href "#"} "Felis euismod semper eget lacinia "
+         [:span {:className "label label-default label-pill"} "#replies"]]]
+       [:p {:className "user"} "username "
+        [:small "Month day, year"] " "
+        [:a {:href "#"}
+         [:span {:className "label label-primary"} "Label"]]]]
+      [:div {:className "articlelisting"}
+       [:p {:className "articlelink"}
+        [:a {:href "#"} "Felis euismod semper eget lacinia "
+         [:span {:className "label label-default label-pill"} "#replies"]]]
+       [:p {:className "user"} "username "
+        [:small "Month day, year"] " "
+        [:a {:href "#"}
+         [:span {:className "label label-primary"} "Label"]]]]
+      [:h5 "Latest Comments "
+       [:small
+        [:small "what your neightbors are saying"]]]
+      [:div {:className "table-responsive"}
+       [:table {:className "table table-striped table-sm articletable"}
+        [:thead
+         [:tr
+          [:th "User"]
+          [:th "Article"]
+          [:th "Label"]
+          [:th "Date\n"]]]
+        [:tbody
+         [:tr
+          [:td "Jen"]
+          [:td "Farmer&#39;s Market Planning"]
+          [:td
+           [:span {:className "label label-default"} "Fruit/Veg"]]
+          [:td "November 1, 2015"]]
+         [:tr
+          [:td "Katie"]
+          [:td "Emerging Leaders Tour Rang-A-Ding Farm"]
+          [:td
+           [:span {:className "label label-default"} "Farming Technology"]
+           [:span {:className "label label-default"} "Fruit/Veg"]]
+          [:td "November 1, 2015"]]
+         [:tr
+          [:td "John"]
+          [:td "NFU Responds to Farm Bill"]
+          [:td
+           [:span {:className "label label-default"} "All"]]
+          [:td "November 1, 2015"]]
+         [:tr {:className "table-success"}
+          [:td "Success"]
+          [:td "Article Name"]
+          [:td
+           [:span {:className "label label-default"} "Label"]]
+          [:td "Month day, year"]]
+         [:tr {:className "table-warning"}
+          [:td "Warning"]
+          [:td "Article Name"]
+          [:td
+           [:span {:className "label label-default"} "Label"]]
+          [:td "Month day, year"]]
+         [:tr {:className "table-danger"}
+          [:td "Danger"]
+          [:td "Article Name"]
+          [:td
+           [:span {:className "label label-default"} "Label"]]
+          [:td "Month day, year"]]
+         [:tr {:className "table-info"}
+          [:td "Info"]
+          [:td "Article Name"]
+          [:td
+           [:span {:className "label label-default"} "Label"]]
+          [:td "Month day, year"]]]]]]]]])
