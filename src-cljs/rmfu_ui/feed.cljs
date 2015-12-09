bec15ab7ecd6c5849d34c08a5d6ca6f7e414b7d7
dviramontes <dviramontes@gmail.com>
2015-12-08 21:14:11 -0700

    adds basic pagination, closes #66

--- src-cljs/rmfu_ui/feed.cljs
+++ src-cljs/rmfu_ui/feed.cljs
@@ -4,21 +4,25 @@
             [ajax.core :refer [GET]]
             [rmfu-ui.utils :refer [get-identity-token]]))
 
-(def ^{:private true} loaded-articles (reagent/atom []))
+(def ^{:private true} article-state (reagent/atom {:offset     0
+                                               :prevOffset 0
+                                               :articles   []}))
 
-(defn- get-articles []
-  (GET "api/articles"
+(def page-size 12)
+
+(defn- get-articles [offset page-size]
+  (GET (str "api/articles?offset=" offset "&page-size=" page-size)
        {:headers         {:identity (get-identity-token)}
         :error-handler   #(js/alert %)
         :response-format :json
         :keywords?       true
         :handler         (fn [res]
-                           (reset! loaded-articles res))}))
+                           (swap! article-state assoc-in [:articles] res))}))
 
 (defn article-nutshell
   "Component for article list item."
   [article id]
-  ^{:key id} 
+  ^{:key id}
   [:div.articlelisting
    [:p.articlelink
     [:a
@@ -31,68 +35,32 @@
 (defn feed []
   (reagent/create-class
     {:component-will-mount
-       #(get-articles)
+     #(get-articles 0 page-size)
+     :component-did-update
+     (fn []
+       (let [{:keys [offset prevOffset]} @article-state]
+         (when-not (= offset prevOffset)
+           (do
+             (get-articles (:offset @article-state) page-size)
+             (swap! article-state assoc :prevOffset offset)))))
      :reagent-render
-       (fn []
-          [:div
-           [:div.container.jumbotron.large-main
-            [:div.row
-             [:div.col-lg-12
-              [:h1.displayinline "Feed"]
-              " "
-              [:h4.displayinline.greytext.pull-right [:a {:href "/#/customfeed"} "Customize"]]
-              [:hr]
-              [:h4 "Articles"]
-              (for [article  @loaded-articles
-                :let  [id (:_id article)]]
-                 (article-nutshell article id))
-              [:h5
-               "Latest Comments "
-               [:small [:small "what your neightbors are saying"]]]
-              [:div.table-responsive
-               [:table.table.table-striped.table-sm.articletable
-                [:thead
-                 [:tr
-                  [:th "User"]
-                  [:th "Article"]
-                  [:th "Label"]
-                  [:th "Date\n                 "]]]
-                [:tbody
-                 [:tr
-                  [:td "Jen"]
-                  [:td "Farmer's Market Planning"]
-                  [:td [:span.label.label-default "Fruit/Veg"]]
-                  [:td "November 1, 2015"]]
-                 [:tr
-                  [:td "Katie"]
-                  [:td "Emerging Leaders Tour Rang-A-Ding Farm"]
-                  [:td
-                   [:span.label.label-default "Farming Technology"]
-                   " "
-                   [:span.label.label-default "Fruit/Veg"]]
-                  [:td "November 1, 2015"]]
-                 [:tr
-                  [:td "John"]
-                  [:td "NFU Responds to Farm Bill"]
-                  [:td [:span.label.label-default "All"]]
-                  [:td "November 1, 2015"]]
-                 [:tr.table-success
-                  [:td "Success"]
-                  [:td "Article Name"]
-                  [:td [:span.label.label-default "Label"]]
-                  [:td "Month day, year"]]
-                 [:tr.table-warning
-                  [:td "Warning"]
-                  [:td "Article Name"]
-                  [:td [:span.label.label-default "Label"]]
-                  [:td "Month day, year"]]
-                 [:tr.table-danger
-                  [:td "Danger"]
-                  [:td "Article Name"]
-                  [:td [:span.label.label-default "Label"]]
-                  [:td "Month day, year"]]
-                 [:tr.table-info
-                  [:td "Info"]
-                  [:td "Article Name"]
-                  [:td [:span.label.label-default "Label"]]
-                  [:td "Month day, year"]]]]]]]]])}))
+     (fn []
+       (let [disable-next (< (count (:articles @article-state)) page-size)]
+         [:div
+          [:div.container.jumbotron.large-main
+           [:div.row
+            [:div.col-lg-12
+             [:h1.displayinline "Feed"]
+             " "
+             [:h4.displayinline.greytext.pull-right [:a {:href "/#/customfeed"} "Customize"]]
+             [:hr]
+             [:h4 "Articles"]
+             (for [article (:articles @article-state)
+                   :let [id (:_id article)]]
+               (article-nutshell article id))
+             [:h5.text-center
+              [:button {:on-click (fn []
+                                    (swap! article-state update :offset #(max (- % page-size) 0)))} "<<"]
+              [:button {:disabled disable-next
+                        :on-click (fn []
+                                    (swap! article-state update :offset #(+ % page-size)))} ">>"]]]]]]))}))
